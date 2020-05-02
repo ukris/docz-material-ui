897bc3d77c31164f9549195bdaa2ba2cc5d910e0
Uma Krishnan <u.krishnan@gmail.com>
2020-05-01 19:26:03 -0700

    remove unwanted files

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -25,26 +25,3 @@ export const hashNumber = (s:string)=>{
   return (h < 0 ? h * -1 : h)
 }
 
-export const downloadAsJSON = (data:any, filename: string) => {
-  if (!data) {
-    console.error('No Data')
-    return
-  }
-  if (!filename) filename = 'download.json'
-  if (typeof data === 'object') {
-    data = JSON.stringify(data, undefined, 4)
-  }
-  const blob = new Blob([data], { type: 'text/json'})
-  const mouseEvent = document.createEvent('MouseEvents')
-  const downloadEl = document.createElement('a')
-
-  downloadEl.download = filename
-  downloadEl.href = window.URL.createObjectURL(blob)
-  downloadEl.dataset.downloadurl = [
-    'text/json',
-    downloadEl.download,
-    downloadEl.href
-  ].join(':')
-  mouseEvent.initMouseEvent('click',true,false,window,0,0,0,0,0,false,false,false,false,0,null)
-  downloadEl.dispatchEvent(mouseEvent)
-}
